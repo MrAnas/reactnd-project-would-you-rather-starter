d26814ec82255c2db8a19da43f6abc697103f812
Anas Fahad AlBassam <analbassam@elm.sa>
2019-05-24 22:12:12 +0300

    removing pages from folders to combine

--- would-you-rather/src/index.js
+++ would-you-rather/src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import store from './containers/_app/store';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import './index.css';
-import App from './containers/_app/App';
-import * as serviceWorker from './serviceWorker';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import reducers from './store'
 
-ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter basename="">
-            <App />
-        </BrowserRouter>
-    </Provider>
-    , document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(reducers,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+);
+
+ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
